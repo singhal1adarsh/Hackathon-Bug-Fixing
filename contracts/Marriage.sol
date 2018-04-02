125b879b00eb2515bfbc5fc7c3d05742c810a39e
satyam <satyam@secureblocks.io>
2018-04-02 11:06:39 +0530

    concept changed

--- contracts/Marriage.sol
+++ contracts/Marriage.sol
@@ -6,9 +6,8 @@ contract Marriage {
     uint256 public lastRegistryNo;
     uint256 public Fee = 1 ether;
     uint256 public feeCollected;
-    uint256 public constant SECURITY = 2 ether;
 
-    enum MarriageStatus { Divorced, Engaged }
+    enum MarriageStatus { Divorced, Engaged, MarriagePending, DivorcedPending }
 
     struct Couple {
         address groom;
@@ -39,19 +38,33 @@ contract Marriage {
      * @return registry no.
      */
     function marriage(address _groom, address _bride) public payable returns(uint256) {
+        require(msg.sender == _groom || msg.sender == _bride);
         /// check whether Groom and bride has already been married or not
-        /// check -- only non married persons voluteer for marriage
-        /// check send value should be equal to -> SECURITY + FEE
+        /// check -- only non-married persons voluteer for marriage
+        /// check send value should be equal to -> FEE otherwise marriage doesn't happened
         feeCollected = feeCollected + Fee;
         lastRegistryNo = lastRegistryNo + 1;
-        /// Pass the Marriage Status below to Engaged
-        coupleData[lastRegistryNo] = Couple(_groom, _bride, MarriageStatus(1), now);
+        /// Pass the Marriage Status below to Pending
+        coupleData[lastRegistryNo] = Couple(_groom, _bride, MarriageStatus(2), now);
         isUserMarried[_groom] = true;
         isUserMarried[_bride] = true;
         /** Emit event to know the blockchain node that they get successful marriage */
         return lastRegistryNo;
     }
 
+
+    /**
+     * @dev Use to approve the request of the bride or groom
+     * @param _registryNo No. of regsitry provided to identify the marriage status
+     * /// Only be called by the lawyer
+     */
+     function approvedRequest(uint256 _registryNo) public {
+         if (coupleData[_registryNo].status == MarriageStatus(2)) {
+             coupleData[_registryNo].status = MarriageStatus(1);
+         } 
+         else
+            coupleData[_registryNo].status = MarriageStatus(0);
+     }
 
     /**
      * @notice Divorced only get validated if only if it is approved by the lawyer
@@ -63,18 +76,14 @@ contract Marriage {
         /** Emit the event to know the blockchain node that they get successful Divorced */
     }
 
-
+    /// @notice only be called by the lawyer
     function withdrawlEther() public returns(bool) {
-        if (msg.sender == lawyer) {
-            /** transfer the feeCollected to the lawyer */
-            feeCollected = 0;
-            return true;
-        } else {
-            /** transfer the Security to the */
-        }
+        // transfer all collected ether to the lawyer
     }
 
-
+    function () {
+        revert();
+    }
 
 
 }
